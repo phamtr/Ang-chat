cbb5f0e5a71a669f47ffb1a28efc525dc18e09ea
Pham Van Truong <pvtruong115@gmail.com>
2019-07-04 14:45:29 +0700

    f

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { ChatService } from '../chat.service';
+import * as moment from 'moment';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-chat';
+  message: string;
+  messages: string[] = [];
+
+  constructor(private chatService: ChatService){
+  }
+  sendMessage() {
+    this.chatService.sendMessage(this.message);
+    this.message = '';
+  }
+  ngOnInit() {
+    this.chatService
+    .getMessages()
+    .subscribe((message: string) => {
+      let currentTime = moment().format('hh:mm:ss a');
+      let messageWithTimestamp =  `${currentTime}: ${message}`;
+      console.log(messageWithTimestamp);
+      
+      this.messages.push(messageWithTimestamp);
+    });
+
+  }
+}
